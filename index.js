02538b8c58d8be0b0c6c04c784179f394d7cd96b
pansatuangsubsin <156601510+pansatuangsubsin@users.noreply.github.com>
2024-09-11 15:47:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 
-function increment() {
+function increment(){
     count += 1
     countEl.textContent = count
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
+function save(){
+    amount = count + " - "
+    saveEl.textContent += amount
     count = 0
+    countEl.textContent = count
 }
+
+
+
+
+
+
+
